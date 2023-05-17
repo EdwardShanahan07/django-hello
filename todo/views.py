db4a50ff1f2a191d4f03a33c104943d572763fdc
Edward Shanahan <edwardshanahan357@gmail.com>
2023-05-17 18:44:52 +0000

    Add homepage

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def get_todo_list(request):
+    return render(request, "todo/todo_list.html",)
